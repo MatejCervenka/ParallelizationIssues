bedc4ecec1dd8c3f8ffd5cedd1ed381fb88e9d90
MatejCervenka <matej.cervenka1106@gmail.com>
2024-12-17 16:35:57 +0100

    Initial commit

---
+++ src/main/java/cz/cervenka/parallelizationissues/ParallelizationIssuesApplication.java
@@ -0,0 +1,13 @@
+package cz.cervenka.parallelizationissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelizationIssuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelizationIssuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/cervenka/parallelizationissues/ParallelizationIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cervenka.parallelizationissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelizationIssuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
